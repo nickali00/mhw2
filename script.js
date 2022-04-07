3e8db779dce841a195cbf7f837db6aa6afd275ca
nickali00 <102171781+nickali00@users.noreply.github.com>
2022-04-07 18:51:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+/* TODO: inserite il codice JavaScript necessario a completare il MHW! */
+
+var sel= new Array();
+function assignSpace()
+{
+  if(event.currentTarget.attributes[2].nodeValue=="one")
+  {
+    pulisci("one");
+    sel[0]=event.currentTarget.attributes[1].nodeValue;
+  }else if(event.currentTarget.attributes[2].nodeValue=="two")
+  {
+   pulisci("two");
+   sel[1]=event.currentTarget.attributes[1].nodeValue;
+  }else{
+    pulisci("three");
+    sel[2]=event.currentTarget.attributes[1].nodeValue;
+  }
+  const new_img = document.createElement('img');
+  new_img.className = "checkbox";
+  new_img.src = 'http://nicolaaliuni.altervista.org/nceck.png';
+  event.currentTarget.classList.remove('cont');
+  event.currentTarget.classList.add('selcol');
+  event.currentTarget.children[1].remove();
+  event.currentTarget.appendChild(new_img);
+  event.currentTarget.removeEventListener('click', assignSpace);
+  selezionato();
+}
+
+
+function attivoev(){
+  var boxes = document.querySelectorAll('.choice-grid .cont');
+  for (const box of boxes)
+  {
+    box.addEventListener('click', assignSpace);
+    freeBoxes.push(box);
+  }
+
+}
+
+
+function pulisci(i){
+  var colo = document.querySelectorAll('.selcol');
+  for (const b of colo)
+  {
+    if(b.attributes[2].nodeValue==i)
+    {
+
+      b.classList.remove('selcol');
+      b.classList.add('cont');
+
+    }
+
+  }
+
+  var boxes = document.querySelectorAll('.choice-grid .cont');
+  for (const box of boxes)
+  {
+    const new_img = document.createElement('img');
+     new_img.src = 'http://nicolaaliuni.altervista.org/cech.png';
+      new_img.className = "checkbox";
+    if(box.attributes[2].nodeValue==i)
+    {
+
+      box.children[1].remove();
+      box.appendChild(new_img);
+      box.addEventListener('click', assignSpace);
+      freeBoxes.push(box);
+    }
+
+  }
+
+}
+
+
+var sem;
+ function selezionato()
+ {
+
+   if(sel[1]!=null && sel[0]!=null && sel[2]!=null )
+   {
+     if( sel[0]===sel[1]){
+       sem=0;
+     }else if (sel[0]===sel[2]) {
+       sem=0;
+     }else if (sel[1]===sel[2]) {
+       sem=1;
+     }else{
+       sem=0;
+     }
+    document.getElementById("bt").disabled = false;
+   }
+
+
+
+
+ }
+
+
+ function risultato()
+ {
+   document.getElementById("reset").disabled = false;
+
+     const resultContainer = document.querySelector('.conten');
+     const header = document.createElement('h1');
+     header.textContent="titolo: "+RESULTS_MAP[sel[sem]].title;
+     resultContainer.appendChild(header);
+     const header2 = document.createElement('p');
+     header2.textContent=" descrizione: "+RESULTS_MAP[sel[sem]].contents;
+     resultContainer.appendChild(header2);
+
+   // Rimuovi gli event listener restanti
+   for (const box of freeBoxes)
+   {
+     box.removeEventListener('click', assignSpace);
+   }
+   document.getElementById("bt").disabled = true;
+ }
+
+ function reset()
+ {
+  document.getElementById("reset").disabled = true;
+  attivoev();
+  pulisci("one");
+  pulisci("two");
+  pulisci("three");
+  const resultContainer = document.querySelector('.conten');
+  resultContainer.innerHTML = '';
+  sel[0]=null;
+  sel[1]=null;
+  sel[2]=null;
+   window.scrollTo(0, 0);
+ }
+
+const freeBoxes = [];
+attivoev();
+document.getElementById("bt").disabled = true;
+document.getElementById("reset").disabled = true;
+var button = document.querySelector("button")
+button.addEventListener("click", risultato);
+var button = document.querySelector("#reset")
+button.addEventListener("click", reset);
